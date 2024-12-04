2fb5005f807e346f6026a3f33234ca1719d321c6
redlintles <treelands54@gmail.com>
2024-12-04 18:50:18 -0300

    Criado Contexto locale

---
+++ src/context/LocaleContext/LocaleContext.ts
@@ -0,0 +1,17 @@
+import { createContext } from "react";
+import PTBRLocale from "../../../locale/PTBR";
+import { GlobalLocale } from "../../types/Locale";
+
+interface LocaleContextState {
+  locale: GlobalLocale;
+  setLocale: React.Dispatch<React.SetStateAction<GlobalLocale>>;
+}
+
+const defaultValue: LocaleContextState = {
+  locale: PTBRLocale,
+  setLocale: () => {},
+};
+
+const localeContext = createContext<LocaleContextState>(defaultValue);
+
+export default localeContext;
---
+++ src/context/LocaleContext/LocaleContext.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import { ReactNode, useState } from "react";
+import PTBRLocale from "../../../locale/PTBR";
+import { GlobalLocale } from "../../types/Locale";
+import localeContext from "./LocaleContext";
+
+export const LocaleContextProvider = ({
+  children,
+}: {
+  children: ReactNode;
+}) => {
+  const [locale, setLocale] = useState<GlobalLocale>(PTBRLocale);
+  return (
+    <localeContext.Provider value={{ locale, setLocale }}>
+      {children}
+    </localeContext.Provider>
+  );
+};
