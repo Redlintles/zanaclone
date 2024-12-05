35f592771efbcca632a2ab43a4b7e5c9df7e9d3d
redlintles <treelands54@gmail.com>
2024-12-05 19:53:19 -0300

    Configurados alguns aliases para simplificar os imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,19 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@hooks": path.resolve(__dirname, "src/hooks"),
+      "@locale": path.resolve(__dirname, "src/locale"),
+      "@layouts": path.resolve(__dirname, "src/layouts"),
+      "@context": path.resolve(__dirname, "src/context"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+    },
+  },
+});
