8d6a93d568e615b9044853168589c288c03cb43d
redlintles <treelands54@gmail.com>
2024-12-05 19:35:10 -0300

    Finalizada Marcação da página laboratórios

--- src/pages/Labs/Labs.tsx
+++ src/pages/Labs/Labs.tsx
@@ -1,5 +1,23 @@
-import React from "react";
+import { useContext } from "react";
+import localeContext, {
+  LocaleContextState,
+} from "../../context/LocaleContext/LocaleContext";
+
+import labsImg from "../../assets/laboratorios.21fae6aa25d15b016974.png";
 
 export default function Labs() {
-  return <div></div>;
+  const { locale } = useContext<LocaleContextState>(localeContext);
+  return (
+    <section className="labs-main">
+      <div className="item item--half">
+        <div className="item-half__left">
+          <h2>{locale.infrastructure.labsPage.title}</h2>
+          <p>{locale.infrastructure.labsPage.child as string}</p>
+        </div>
+        <div className="item-half__right">
+          <img src={labsImg} alt="" />
+        </div>
+      </div>
+    </section>
+  );
 }
