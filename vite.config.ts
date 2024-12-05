6978811582e46f7ad0fec1444044f5399ec2ab33
redlintles <treelands54@gmail.com>
2024-12-05 20:50:02 -0300

    Adicionado plugin para lidar com os aliases do vite, configurado alias para lidar com tipos

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
+import tsConfigPaths from "vite-tsconfig-paths";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tsConfigPaths()],
   resolve: {
     alias: {
       "@assets": path.resolve(__dirname, "src/assets"),
