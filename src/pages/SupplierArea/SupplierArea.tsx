c0b3fc3594dc09be8bcacb3146a3feac26eb1cf1
redlintles <treelands54@gmail.com>
2024-12-06 19:38:51 -0300

    Finalizada Marcação da página SupplierArea

--- src/pages/SupplierArea/SupplierArea.tsx
+++ src/pages/SupplierArea/SupplierArea.tsx
@@ -1,5 +1,32 @@
-import React from "react";
+import { LocaleContextState } from "@app-types/locale";
+import React, { useContext } from "react";
+import localeContext from "../../context/LocaleContext/LocaleContext";
 
 export default function SupplierArea() {
-  return <div></div>;
+  const { locale } = useContext<LocaleContextState>(localeContext);
+  return (
+    <section className="supplya-main">
+      <div className="supplya-main__video">
+        <video src="" className="supplya__video">
+          <p>Não foi possível carregar o vídeo</p>
+        </video>
+      </div>
+      <article className="item item--sm ">
+        <h2>{locale.qualityAndEnvironment.supplierArea.item.title}</h2>
+        <p>{locale.qualityAndEnvironment.supplierArea.item.child as string}</p>
+      </article>
+
+      <div className="supplya-refs outline">
+        <a href="#" className="supplya-refs__link">
+          {locale.qualityAndEnvironment.supplierArea.btn1Text}
+        </a>
+        <a href="#" className="supplya-refs__link">
+          {locale.qualityAndEnvironment.supplierArea.btn2Text}
+        </a>
+        <a href="#" className="supplya-refs__link">
+          {locale.qualityAndEnvironment.supplierArea.btn3Text}
+        </a>
+      </div>
+    </section>
+  );
 }
