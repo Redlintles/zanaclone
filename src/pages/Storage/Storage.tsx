2e8cca0a0a5ab17c3a2e350d80f1789e06c50790
redlintles <treelands54@gmail.com>
2024-12-06 20:26:39 -0300

    Finalizada Marcação da página Storage

--- src/pages/Storage/Storage.tsx
+++ src/pages/Storage/Storage.tsx
@@ -1,5 +1,27 @@
-import React from "react";
-
+import { useContext } from "react";
+import ItemHalf from "@components/ItemHalf/ItemHalf";
+import { LocaleContextState } from "@app-types/locale";
+import localeContext from "@context/LocaleContext/LocaleContext";
+import storageImg from "@assets/armazenamento.bb0001c11eab834e9793.png";
 export default function Storage() {
-  return <div></div>;
+  const { locale } = useContext<LocaleContextState>(localeContext);
+  return (
+    <>
+      <div className="storage-main">
+        <ItemHalf
+          leftElement={
+            <>
+              <h2>{locale.logistics.storage.title}</h2>
+              <p>{locale.logistics.storage.child as string}</p>
+            </>
+          }
+          rightElement={
+            <>
+              <img src={storageImg} alt="" className="storage-main__img" />
+            </>
+          }
+        />
+      </div>
+    </>
+  );
 }
