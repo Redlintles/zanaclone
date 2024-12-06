4a83156a419d9cf8a3586e026008a00aa4d482fa
redlintles <treelands54@gmail.com>
2024-12-06 20:11:48 -0300

    Finalizada marcação da página FISPQS

--- src/pages/FISPQS/FISPQS.tsx
+++ src/pages/FISPQS/FISPQS.tsx
@@ -1,5 +1,27 @@
-import React from "react";
+import { useContext } from "react";
+import { LocaleContextState } from "@app-types/locale";
+import localeContext from "../../context/LocaleContext/LocaleContext";
+import ItemList from "../../components/ItemList/ItemList";
 
 export default function FISPQS() {
-  return <div></div>;
+  const { locale } = useContext<LocaleContextState>(localeContext);
+  return (
+    <>
+      <div className="fispqs-main">
+        <section className="fisps-main__text">
+          <h2>{locale.qualityAndEnvironment.fispsqsArea.item.title}</h2>
+          <p>{locale.qualityAndEnvironment.fispsqsArea.item.child as string}</p>
+        </section>
+
+        <section className="fisps-main__container ">
+          <ItemList
+            items={locale.qualityAndEnvironment.fispsqsArea.sublinks1}
+          />
+          <ItemList
+            items={locale.qualityAndEnvironment.fispsqsArea.sublinks2}
+          />
+        </section>
+      </div>
+    </>
+  );
 }
