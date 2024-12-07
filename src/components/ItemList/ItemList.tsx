5e31822464389729c7896a9ba3cabb026b763ed0
redlintles <treelands54@gmail.com>
2024-12-07 14:48:55 -0300

    Adicionados Estilos à ItemList

--- src/components/ItemList/ItemList.tsx
+++ src/components/ItemList/ItemList.tsx
@@ -1,15 +1,39 @@
+import styled from "styled-components";
+import ItemText from "../ItemText/ItemText";
+
 interface ItemListProps {
   items: string[];
 }
 
+const StyledItemList = styled.ul`
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+  padding-left: 1rem;
+  & > li {
+    position: relative;
+
+    &::before {
+      position: absolute;
+      top: 0.75rem;
+      left: -1rem;
+      content: "";
+      width: 0.35rem;
+      height: 0.35rem;
+      border-radius: 50%;
+      background-color: var(--color-gray);
+    }
+  }
+`;
+
 export default function ItemList({ items }: ItemListProps) {
   return (
-    <ul className="item-list">
+    <StyledItemList>
       {items.map((item, index) => (
         <li key={index} className="item-list__item">
-          <p className="item-list__text">{item}</p>
+          <ItemText>{item}</ItemText>
         </li>
       ))}
-    </ul>
+    </StyledItemList>
   );
 }
