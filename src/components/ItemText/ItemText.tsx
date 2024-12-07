97a33f8aa699df7d319eaa5e51e90127f835deee
redlintles <treelands54@gmail.com>
2024-12-07 14:47:18 -0300

    Criado Componente ItemText

---
+++ src/components/ItemText/ItemText.tsx
@@ -0,0 +1,14 @@
+import { ReactNode } from "react";
+import styled from "styled-components";
+
+interface ItemTextProps {
+  children: ReactNode;
+}
+
+const StyledItemText = styled.p`
+  color: var(--color-gray);
+  line-height: 1.75rem;
+`;
+export default function ItemText({ children }: ItemTextProps) {
+  return <StyledItemText>{children}</StyledItemText>;
+}
