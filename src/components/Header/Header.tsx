561935cee3df3df88cdb9c0728701aa20326f8d3
redlintles <treelands54@gmail.com>
2024-12-07 13:48:05 -0300

    Finalizada estilização de Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -10,9 +10,129 @@ import logoZanaflex from "@assets/logo-zanaflex.png";
 import { FaCaretDown } from "react-icons/fa6";
 import { Link } from "react-router-dom";
 
-const TopHeader = styled.div``;
-const SubHeader = styled.div``;
+const TopHeader = styled.header`
+  display: flex;
+  justify-content: space-between;
+  padding: 1.5rem 22.5rem;
+  background-color: var(--color-gray);
+  align-items: center;
+
+  .header__logo {
+    width: 303px;
+  }
+
+  .header__logo > img {
+    width: 100%;
+  }
+`;
+const SubHeader = styled.nav`
+  display: flex;
+  background-color: var(--color-dark-bg);
+  padding: 1rem 22.5rem;
+  gap: 2rem;
+  justify-content: center;
+
+  .header-link {
+    position: relative;
+    color: white;
+    border-radius: 5px;
+    padding: 1rem 0.5rem;
+    transition: 0.25s ease background-color;
+
+    &:hover {
+      background-color: var(--color-yellow);
+    }
+
+    &__link {
+      display: flex;
+      align-items: center;
+      gap: 0.25rem;
+    }
+
+    &__link::before {
+      content: "";
+      position: absolute;
+      top: 0;
+      left: 0;
+      bottom: 0;
+      right: 0;
+    }
+
+    a {
+      color: inherit;
+      font-size: 0.85rem;
+    }
+
+    &__sublinks {
+      position: absolute;
+      display: flex;
+      flex-direction: column;
+      margin-top: 2rem;
+      left: 0;
+      opacity: 0;
+      visibility: hidden;
+      width: max-content;
+      min-width: 200%;
+      background-color: var(--color-yellow);
+      z-index: 10;
+
+      &:hover {
+        opacity: 1;
+        visibility: visible;
+      }
+    }
+
+    &__sublinks::before {
+      position: absolute;
+      content: "";
+      top: -2rem;
+      z-index: -1;
+      left: 0;
+      right: 0;
+      bottom: 0;
+    }
+
+    &:hover > .header-link__sublinks {
+      opacity: 1;
+      visibility: visible;
+    }
+
+    &__sublink {
+      white-space: nowrap;
+      padding: 1.5rem 1rem;
+
+      &:hover {
+        background-color: var(--color-yellow-dark);
+      }
+    }
+  }
+`;
 const MainHeader = styled.header``;
+const HeaderCountries = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 1rem;
+
+  & > span {
+    color: white;
+    font-size: 0.75rem;
+  }
+  & > div {
+    display: flex;
+    gap: 1rem;
+
+    & > button {
+      width: 1.2rem;
+      cursor: pointer;
+      background-color: var(--color-gray);
+    }
+    .header-countries__country {
+      width: 100%;
+    }
+  }
+`;
 
 export default function Header() {
   const { locale } = useContext<LocaleContextState>(localeContext);
@@ -24,18 +144,32 @@ export default function Header() {
             <img src={logoZanaflex} alt="" />
           </div>
           <div className="header__right">
-            <span>{locale.type}</span>
-            <div className="header__countries">
-              <button>
-                <img src={bandeiraBrasil} alt="" />
-              </button>
-              <button>
-                <img src={bandeiraEspanha} alt="" />
-              </button>
-              <button>
-                <img src={bandeiraEua} alt="" />
-              </button>
-            </div>
+            <HeaderCountries>
+              <span>{locale.type}</span>
+              <div>
+                <button>
+                  <img
+                    src={bandeiraBrasil}
+                    alt="Uma bandeira do Brasil"
+                    className="header-countries__country"
+                  />
+                </button>
+                <button>
+                  <img
+                    src={bandeiraEspanha}
+                    alt="Um Bandeira da Espanha"
+                    className="header-countries__country"
+                  />
+                </button>
+                <button>
+                  <img
+                    src={bandeiraEua}
+                    alt="Uma Bandeira dos EUA"
+                    className="header-countries__country"
+                  />
+                </button>
+              </div>
+            </HeaderCountries>
           </div>
         </TopHeader>
         <SubHeader>
@@ -51,7 +185,11 @@ export default function Header() {
                   <div className="header-link__sublinks">
                     {item.sublinks &&
                       item.sublinks.map((subItem, index) => (
-                        <Link to={item.to + subItem.to} key={index}>
+                        <Link
+                          to={item.to + subItem.to}
+                          key={index}
+                          className="header-link__sublink"
+                        >
                           {subItem.text}
                         </Link>
                       ))}
