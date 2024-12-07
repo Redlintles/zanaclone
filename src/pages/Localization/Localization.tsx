6abf27ac99ce3659f3d2bc62e7f1234504affea9
redlintles <treelands54@gmail.com>
2024-12-06 21:35:05 -0300

    Finalizada Marcação em Localization

--- src/pages/Localization/Localization.tsx
+++ src/pages/Localization/Localization.tsx
@@ -1,5 +1,24 @@
 import React from "react";
 
 export default function Localization() {
-  return <div></div>;
+  return (
+    <>
+      <div className="localization-main">
+        <section className="localization-main__map">
+          <iframe src="">
+            <p>Este iframe não pode ser exibido</p>
+          </iframe>
+        </section>
+        <section className="localization-main__text">
+          <p>Zanaflex &reg;</p>
+          <p>Zanaflex Borrachas Ltda.</p>
+          <p>Av. Vasco Massafeli, 1010</p>
+          <p>Cotia - SP - Brasil - CEP 06703-600</p>
+          <p>Fone : +55 (11) 2165-3450</p>
+          <p>Fax : +55 (11) 2165-3451</p>
+          <a href="/">www.zanaflex.com.br</a>
+        </section>
+      </div>
+    </>
+  );
 }
