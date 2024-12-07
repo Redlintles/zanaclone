37c66c8dbf015d02f2d717761dedb85b151adcd2
redlintles <treelands54@gmail.com>
2024-12-06 21:09:01 -0300

    Corrigido erro de ausência de Outlet em ProdcutsLayout

--- src/layouts/ProductsLayout.tsx
+++ src/layouts/ProductsLayout.tsx
@@ -3,12 +3,14 @@ import SubNav from "../components/SubNav/SubNav";
 import localeContext, {
   LocaleContextState,
 } from "../context/LocaleContext/LocaleContext";
+import { Outlet } from "react-router-dom";
 
 export default function ProductsLayout() {
   const { locale } = useContext<LocaleContextState>(localeContext);
   return (
     <main className="main">
       <SubNav links={[locale.products.sublink]} />
+      <Outlet />
     </main>
   );
 }
