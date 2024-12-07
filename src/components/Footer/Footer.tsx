74f32d071ea6b19041b1ee00c1b69f279761c0ab
redlintles <treelands54@gmail.com>
2024-12-07 13:48:20 -0300

    Finalizada a estilização de Footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,23 @@
-import React from "react";
+import { LocaleContextState } from "@app-types/locale";
+import { useContext } from "react";
+import styled from "styled-components";
+import localeContext from "../../context/LocaleContext/LocaleContext";
 
+const StyledFooter = styled.footer`
+  background-color: var(--color-dark-bg);
+  padding: 2rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-aligin: center;
+  color: white;
+  font-size: 0.9rem;
+`;
 export default function Footer() {
-  return <div></div>;
+  const { locale } = useContext<LocaleContextState>(localeContext);
+  return (
+    <StyledFooter>
+      <p>{locale.footer.text}</p>
+    </StyledFooter>
+  );
 }
