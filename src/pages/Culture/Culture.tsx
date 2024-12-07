d5069b40577b71303d2a3ea27a42014ff95400ca
redlintles <treelands54@gmail.com>
2024-12-07 19:08:33 -0300

    Finalizada Estilização da página Culture

--- src/pages/Culture/Culture.tsx
+++ src/pages/Culture/Culture.tsx
@@ -1,15 +1,16 @@
-import { useContext } from "react";
-import SubNav from "@components/SubNav/SubNav";
 import cultureImg from "@assets/cultura.bcf7316d55e0217a58e8.png";
-import localeContext from "@context/LocaleContext/LocaleContext";
-import { LocaleContextState } from "@app-types/locale";
+import ItemImg from "../../components/ItemImg/ItemImg";
+import styled from "styled-components";
+
+const StyledCulture = styled.section`
+  width: 100%;
+  height: 120vh;
+`;
 
 export default function Culture() {
-  const { locale } = useContext<LocaleContextState>(localeContext);
   return (
-    <main className="culture main">
-      <SubNav links={locale.company.sublinks} />
-      <img src={cultureImg} alt="" />
-    </main>
+    <StyledCulture>
+      <ItemImg src={cultureImg} />
+    </StyledCulture>
   );
 }
