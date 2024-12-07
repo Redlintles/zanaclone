dbf182797dce007ab3dbddd56decd9c2917b7632
redlintles <treelands54@gmail.com>
2024-12-06 21:47:23 -0300

    Finalizada Marcação de Links

--- src/pages/Links/Links.tsx
+++ src/pages/Links/Links.tsx
@@ -1,5 +1,31 @@
-import React from "react";
+import { LocaleContextState } from "@app-types/locale";
+import { useContext } from "react";
+import localeContext from "../../context/LocaleContext/LocaleContext";
+import { LinkCalls } from "@app-types/locale";
 
 export default function Links() {
-  return <div></div>;
+  const { locale } = useContext<LocaleContextState>(localeContext);
+  return (
+    <>
+      <section className="links-main">
+        <h2>{locale.links.title}</h2>
+
+        <div>
+          <ul>
+            {(locale.links.child as Array<LinkCalls>).map((item, index) => (
+              <li key={index}>
+                <p>{item.text}</p>
+
+                {item.links.map((link, index) => (
+                  <a key={index} href={link.to}>
+                    {link.text}
+                  </a>
+                ))}
+              </li>
+            ))}
+          </ul>
+        </div>
+      </section>
+    </>
+  );
 }
