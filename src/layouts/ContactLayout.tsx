6bc7b20f0c35b12f5e8d5b51d0919ff034940da7
redlintles <treelands54@gmail.com>
2024-12-06 21:31:12 -0300

    Adicionado Outlet em ContactLayout

--- src/layouts/ContactLayout.tsx
+++ src/layouts/ContactLayout.tsx
@@ -3,12 +3,14 @@ import localeContext, {
   LocaleContextState,
 } from "../context/LocaleContext/LocaleContext";
 import SubNav from "../components/SubNav/SubNav";
+import { Outlet } from "react-router-dom";
 
 export default function ContactLayout() {
   const { locale } = useContext<LocaleContextState>(localeContext);
   return (
     <main className="main">
       <SubNav links={locale.contactLocale.sublinks} />
+      <Outlet />
     </main>
   );
 }
