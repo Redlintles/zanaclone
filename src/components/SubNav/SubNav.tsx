426df1f181a1ea27b57caab15312583c03c91d8e
redlintles <treelands54@gmail.com>
2024-12-07 16:54:10 -0300

    Corrigido bug de roteamento em SubNav

--- src/components/SubNav/SubNav.tsx
+++ src/components/SubNav/SubNav.tsx
@@ -1,19 +1,32 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import styled from "styled-components";
 
 interface SubNavProps {
   links: Array<{ text: string; to: string }>;
 }
 
+const StyledSubNav = styled.nav``;
+
 export default function SubNav({ links }: SubNavProps) {
+  const { pathname } = useLocation();
+
+  const match = pathname.match(new RegExp("^(/[^/]+)"));
+  let superLink = "";
+  if (match) {
+    superLink = match[1];
+  }
   return (
-    <nav className={"main-subnav"}>
+    <StyledSubNav>
       {links &&
         links.map((item, index) => (
-          <Link key={index} className="main-subnav__link" to={item.to}>
+          <Link
+            key={index}
+            className="main-subnav__link"
+            to={superLink + item.to}
+          >
             {item.text}
           </Link>
         ))}
-    </nav>
+    </StyledSubNav>
   );
 }
