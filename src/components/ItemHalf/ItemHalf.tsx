d683ab78e7168b4ffd377873b2241ec368e2d54d
redlintles <treelands54@gmail.com>
2024-12-07 14:48:36 -0300

    Adicionada Opção BorderBottom em ItemHalf

--- src/components/ItemHalf/ItemHalf.tsx
+++ src/components/ItemHalf/ItemHalf.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from "react";
+import { ReactNode } from "react";
 import styled from "styled-components";
 
 const ItemHalfContainer = styled.div`
@@ -6,19 +6,35 @@ const ItemHalfContainer = styled.div`
   grid-template-columns: repeat(2, 1fr);
   width: 100%;
   gap: 2rem;
+  padding: 2rem 0;
 
   & > * {
     grid-column: span 1;
+  }
+
+  &.item-half--border-bottom {
+    border-bottom: 5px solid var(--color-gray);
   }
 `;
 
 interface ItemHalfProps {
   leftElement: ReactNode;
   rightElement: ReactNode;
+  borderBottom?: boolean;
 }
-export default function ItemHalf({ leftElement, rightElement }: ItemHalfProps) {
+export default function ItemHalf({
+  leftElement,
+  rightElement,
+  borderBottom,
+}: ItemHalfProps) {
+  let fullClassName: string = "";
+
+  if (borderBottom) {
+    fullClassName += "item-half--border-bottom";
+  }
+
   return (
-    <ItemHalfContainer>
+    <ItemHalfContainer className={fullClassName}>
       <div className="item-half__left">{leftElement}</div>
       <div className="item-half__right">{rightElement}</div>
     </ItemHalfContainer>
