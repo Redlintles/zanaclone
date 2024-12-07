f912a78278a0405794c8d7f43c1797944e195daf
redlintles <treelands54@gmail.com>
2024-12-07 19:31:19 -0300

    Adicionado um texto justificado à ItemText

--- src/components/ItemText/ItemText.tsx
+++ src/components/ItemText/ItemText.tsx
@@ -8,6 +8,7 @@ interface ItemTextProps {
 const StyledItemText = styled.p`
   color: var(--color-gray);
   line-height: 1.75rem;
+  text-align: justify;
 `;
 export default function ItemText({ children }: ItemTextProps) {
   return <StyledItemText>{children}</StyledItemText>;
