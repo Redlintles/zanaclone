9d7dbcb557071443b5343e68c2876e70949af39e
redlintles <treelands54@gmail.com>
2024-12-09 20:11:16 -0300

    Finalizada estilização provisória de Links

--- src/pages/Links/Links.tsx
+++ src/pages/Links/Links.tsx
@@ -2,30 +2,65 @@ import { LocaleContextState } from "@app-types/locale";
 import { useContext } from "react";
 import localeContext from "@context/LocaleContext/LocaleContext";
 import { LinkCalls } from "@app-types/locale";
+import Container from "../../components/Container/Container";
+import ItemTitle from "../../components/ItemTitle/ItemTitle";
+import styled from "styled-components";
+import ItemText from "../../components/ItemText/ItemText";
+import { Link } from "react-router-dom";
+import ItemElement from "../../components/ItemElement/ItemElement";
 
+const StyledLinkList = styled.ul`
+  padding-left: 2rem;
+
+  & > li {
+    display: flex;
+    align-items: center;
+    gap: 0.5rem;
+    position: relative;
+
+    &::before {
+      --size: 5px;
+      content: "";
+      position: absolute;
+      top: calc(50% - calc(var(--size) / 2));
+      left: calc(var(--size) * -3);
+      width: var(--size);
+      height: var(--size);
+      background-color: var(--color-gray);
+      border-radius: 50%;
+    }
+
+    & > a {
+      color: var(--color-gray);
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+  }
+`;
 export default function Links() {
   const { locale } = useContext<LocaleContextState>(localeContext);
   return (
-    <>
+    <Container>
       <section className="links-main">
-        <h2>{locale.links.title}</h2>
+        <ItemTitle borderBottom>{locale.links.title}</ItemTitle>
 
-        <div>
-          <ul>
+        <ItemElement>
+          <StyledLinkList>
             {(locale.links.child as Array<LinkCalls>).map((item, index) => (
               <li key={index}>
-                <p>{item.text}</p>
+                <ItemText>{item.text}</ItemText>
 
                 {item.links.map((link, index) => (
-                  <a key={index} href={link.to}>
+                  <Link key={index} to={link.to}>
                     {link.text}
-                  </a>
+                  </Link>
                 ))}
               </li>
             ))}
-          </ul>
-        </div>
+          </StyledLinkList>
+        </ItemElement>
       </section>
-    </>
+    </Container>
   );
 }
