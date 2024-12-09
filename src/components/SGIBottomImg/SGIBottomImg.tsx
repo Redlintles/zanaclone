d335c6229183df3ee52eb088c3ef8d4a15fb874f
redlintles <treelands54@gmail.com>
2024-12-09 16:22:47 -0300

    Criado componente SGIBottomImg

--- src/components/SGIBottomImg/SGIBottomImg.tsx
+++ src/components/SGIBottomImg/SGIBottomImg.tsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 interface SgiBottomImgProps {
@@ -5,7 +6,44 @@ interface SgiBottomImgProps {
   links?: { to: string; top: string[]; bottom: string };
 }
 
-const StyledSgiBottomImg = styled.div``;
+const StyledSgiBottomImg = styled.div`
+  position: relative;
+
+  & > .img-wrapper__text {
+    position: absolute;
+    top: 10%;
+    left: 10%;
+    width: 80%;
+    height: 80%;
+    background-color: rgba(0, 0, 0, 0.4);
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    border: 5px solid white;
+    padding: 1rem;
+    opacity: 0;
+    visibility: hidden;
+    transition: 0.5s ease opacity, 0.5s ease visibility;
+
+    a {
+      color: white;
+
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+
+    & > .img-wrapper__link-top {
+      display: flex;
+      flex-direction: column;
+    }
+  }
+
+  &:hover > .img-wrapper__text {
+    opacity: 1;
+    visibility: visible;
+  }
+`;
 
 export default function SgiBottomImg({ src, links }: SgiBottomImgProps) {
   return (
@@ -16,11 +54,11 @@ export default function SgiBottomImg({ src, links }: SgiBottomImgProps) {
         <div className="img-wrapper__text">
           <div className="img-wrapper__link-top">
             {links.top.map((item) => (
-              <a href={links.to}>{item}</a>
+              <Link to={links.to}>{item}</Link>
             ))}
           </div>
           <div className="img-wrapper__link-bottom">
-            <a href={links.to}>{links.bottom}</a>
+            <Link to={links.to}>{links.bottom}</Link>
           </div>
         </div>
       )}
