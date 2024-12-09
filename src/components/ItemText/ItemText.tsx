08315d33adcb98d58a83ac058b1950693066b175
redlintles <treelands54@gmail.com>
2024-12-09 16:31:22 -0300

    Novas funcionalidades adicionas a ItemText

--- src/components/ItemText/ItemText.tsx
+++ src/components/ItemText/ItemText.tsx
@@ -1,15 +1,29 @@
 import { ReactNode } from "react";
 import styled from "styled-components";
+import useClassManager from "../../hooks/useClassManager";
 
 interface ItemTextProps {
   children: ReactNode;
+  textCenter?: boolean;
+  fontSize?: string;
 }
 
 const StyledItemText = styled.p`
   color: var(--color-gray);
   line-height: 1.75rem;
   text-align: justify;
+
+  &.item__text--center {
+    text-align: center;
+  }
 `;
-export default function ItemText({ children }: ItemTextProps) {
-  return <StyledItemText>{children}</StyledItemText>;
+export default function ItemText({
+  children,
+  textCenter,
+  fontSize,
+}: ItemTextProps) {
+  const [manager] = useClassManager("", []);
+
+  manager.append([textCenter, "item__text--center"]);
+  return <StyledItemText style={{ fontSize }}>{children}</StyledItemText>;
 }
