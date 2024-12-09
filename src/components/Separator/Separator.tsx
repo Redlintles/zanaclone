600ffb8f1808e201b1f0ac10567c62bf51bb401a
redlintles <treelands54@gmail.com>
2024-12-09 18:36:08 -0300

    Adicionado componente Separator

---
+++ src/components/Separator/Separator.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const StyledSeparator = styled.span`
+  width: 100%;
+  height: 5px;
+  background-color: var(--color-gray);
+  margin: 2rem auto;
+`;
+
+interface SeparatorProps {
+  margin?: string;
+  width?: string;
+}
+
+export default function Separator({ margin, width }: SeparatorProps) {
+  return <StyledSeparator style={{ margin, width }} />;
+}
