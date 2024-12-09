e7aab58745f8690a5bf1d3f2548e445aed0897bf
redlintles <treelands54@gmail.com>
2024-12-09 17:43:56 -0300

    Finalizada estilização provisória  de FISPQS

--- src/pages/FISPQS/FISPQS.tsx
+++ src/pages/FISPQS/FISPQS.tsx
@@ -2,26 +2,42 @@ import { useContext } from "react";
 import { LocaleContextState } from "@app-types/locale";
 import localeContext from "@context/LocaleContext/LocaleContext";
 import ItemList from "@components/ItemList/ItemList";
+import ItemTitle from "../../components/ItemTitle/ItemTitle";
+import ItemText from "../../components/ItemText/ItemText";
+import Container from "../../components/Container/Container";
+import ItemElement from "../../components/ItemElement/ItemElement";
 
 export default function FISPQS() {
   const { locale } = useContext<LocaleContextState>(localeContext);
   return (
-    <>
+    <Container padding="0 18rem">
       <div className="fispqs-main">
         <section className="fisps-main__text">
-          <h2>{locale.qualityAndEnvironment.fispsqsArea.item.title}</h2>
-          <p>{locale.qualityAndEnvironment.fispsqsArea.item.child as string}</p>
+          <ItemTitle padding="0" margin="0 auto 2rem">
+            {locale.qualityAndEnvironment.fispsqsArea.item.title}
+          </ItemTitle>
+          <ItemText fontSize="1.3rem" textCenter>
+            {locale.qualityAndEnvironment.fispsqsArea.item.child as string}
+          </ItemText>
         </section>
 
-        <section className="fisps-main__container ">
+        <ItemElement
+          outline
+          directionRow
+          width="60%"
+          margin="4rem auto"
+          padding="4rem 0"
+        >
           <ItemList
+            gap="2rem"
             items={locale.qualityAndEnvironment.fispsqsArea.sublinks1}
           />
           <ItemList
+            gap="2rem"
             items={locale.qualityAndEnvironment.fispsqsArea.sublinks2}
           />
-        </section>
+        </ItemElement>
       </div>
-    </>
+    </Container>
   );
 }
