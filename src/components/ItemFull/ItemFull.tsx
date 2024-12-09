f52edda14dc41975379cce1d73a64584f16bc421
redlintles <treelands54@gmail.com>
2024-12-09 17:33:12 -0300

    Adicionadas propriedades de controle de borda e box model em ItemFull

--- src/components/ItemFull/ItemFull.tsx
+++ src/components/ItemFull/ItemFull.tsx
@@ -1,5 +1,6 @@
 import { ReactNode } from "react";
 import styled from "styled-components";
+import useClassManager from "../../hooks/useClassManager";
 
 const StyledItemFull = styled.article`
   padding: 4rem 1rem;
@@ -9,12 +10,37 @@ const StyledItemFull = styled.article`
   justify-content: center;
   border-top: 5px solid var(--color-gray);
   border-bottom: 5px solid var(--color-gray);
+
+  &.item-full__b-top-none {
+    border-top: none;
+  }
+  &.item-full__b-bottom-none {
+    border-bottom: none;
+  }
 `;
 
 interface ItemFullProps {
   children: ReactNode;
+  borderTopNone?: boolean;
+  borderBottomNone?: boolean;
+  margin?: string;
+  padding?: string;
 }
 
-export default function ItemFull({ children }: ItemFullProps) {
-  return <StyledItemFull>{children}</StyledItemFull>;
+export default function ItemFull({
+  children,
+  borderTopNone,
+  borderBottomNone,
+  margin,
+  padding,
+}: ItemFullProps) {
+  const [manager] = useClassManager("", []);
+
+  manager.append([borderTopNone, "item-full__b-top-none"]);
+  manager.append([borderBottomNone, "item-full__b-bottom-none"]);
+  return (
+    <StyledItemFull style={{ margin, padding }} className={manager.getResult()}>
+      {children}
+    </StyledItemFull>
+  );
 }
