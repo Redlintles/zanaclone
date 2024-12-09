8884cdc36e140822189289c06feae7f4ca3b476f
redlintles <treelands54@gmail.com>
2024-12-09 16:31:08 -0300

    Novas funcionalidades adicionadas a ItemTitle

--- src/components/ItemTitle/ItemTitle.tsx
+++ src/components/ItemTitle/ItemTitle.tsx
@@ -1,22 +1,43 @@
 import { ReactNode } from "react";
 import styled from "styled-components";
+import useClassManager from "../../hooks/useClassManager";
 
 const StyledItemTitle = styled.h2`
   color: var(--color-gray);
   font-size: 1.5rem;
   padding-bottom: 2rem;
-  border-bottom: 4px solid var(--color-gray);
+
+  &.item__title-b-bottom {
+    border-bottom: 4px solid var(--color-gray);
+  }
+
+  &.item__title--bold {
+    font-weight: 450;
+  }
   text-align: center;
   line-height: 40px;
-  font-weight: 450;
   width: fit-content;
   margin: 0 auto 2rem;
 `;
 
 interface ItemTitleProps {
   children: ReactNode;
+  borderBottom?: boolean;
+  bold?: boolean;
 }
 
-export default function ItemTitle({ children }: ItemTitleProps) {
-  return <StyledItemTitle>{children}</StyledItemTitle>;
+export default function ItemTitle({
+  children,
+  borderBottom,
+  bold,
+}: ItemTitleProps) {
+  const [manager] = useClassManager("", [
+    [borderBottom, "item__title--b-bottom"],
+    [bold, "item__title--bold"],
+  ]);
+  return (
+    <StyledItemTitle className={manager.getResult()}>
+      {children}
+    </StyledItemTitle>
+  );
 }
