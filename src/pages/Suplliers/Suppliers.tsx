d90fe8948b6105b4738ea27e892c7fb34d4533c2
redlintles <treelands54@gmail.com>
2024-12-09 18:18:03 -0300

    Finalizada estilização provisória de Suppliers

--- src/pages/Suplliers/Suppliers.tsx
+++ src/pages/Suplliers/Suppliers.tsx
@@ -2,19 +2,34 @@ import { LocaleContextState } from "@app-types/locale";
 import React, { useContext } from "react";
 import localeContext from "@context/LocaleContext/LocaleContext";
 import supplyMap from "@assets/fornecedores-map.b685947c51d81821e53a.png";
+import Container from "../../components/Container/Container";
+import ItemTitle from "../../components/ItemTitle/ItemTitle";
+import ItemText from "../../components/ItemText/ItemText";
+import styled from "styled-components";
+
+const StyledSupplyMainMap = styled.section`
+  margin-top: 2rem;
+  & > img {
+    width: 100%;
+  }
+`;
 export default function Suppliers() {
   const { locale } = useContext<LocaleContextState>(localeContext);
   return (
-    <>
+    <Container>
       <div className="supply-main">
         <section className="supply-main__text">
-          <h2>{locale.logistics.item.title}</h2>
-          <p>{locale.logistics.item.child as string}</p>
+          <ItemTitle borderBottom bold>
+            {locale.logistics.item.title}
+          </ItemTitle>
+          <ItemText textCenter>
+            {locale.logistics.item.child as string}
+          </ItemText>
         </section>
-        <section className="supply-main__map">
+        <StyledSupplyMainMap>
           <img src={supplyMap} alt="" className="supply-man__img" />
-        </section>
+        </StyledSupplyMainMap>
       </div>
-    </>
+    </Container>
   );
 }
