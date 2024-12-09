cfa70f526519d7cf2609f653ef4b681533a4a761
redlintles <treelands54@gmail.com>
2024-12-09 18:37:13 -0300

    Finalizada estilização provisória de CompoundsOutput

--- src/pages/CompoundsOutput/CompoundsOutput.tsx
+++ src/pages/CompoundsOutput/CompoundsOutput.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import { useContext } from "react";
 import ItemHalf from "@components/ItemHalf/ItemHalf";
 import { LocaleContextState } from "@app-types/locale";
 import localeContext from "@context/LocaleContext/LocaleContext";
@@ -10,32 +10,49 @@ import slideImg4 from "@assets/4.9c471b7742942084ac50.png";
 import slideImg5 from "@assets/5.0803fb2f6cb1e905bde4.png";
 
 import rightImg from "@assets/embalagens.57fd184c1374a660b29a.png";
+import Container from "../../components/Container/Container";
+import ItemTitle from "../../components/ItemTitle/ItemTitle";
+import ItemText from "../../components/ItemText/ItemText";
+import ItemElement from "../../components/ItemElement/ItemElement";
+import Separator from "../../components/Separator/Separator";
+import ItemImg from "../../components/ItemImg/ItemImg";
 export default function CompoundsOutput() {
   const { locale } = useContext<LocaleContextState>(localeContext);
   return (
-    <>
+    <Container>
       <section className="compounds-o-main">
         <ItemHalf
           leftElement={
-            <>
-              <h2>{locale.logistics.compoundOutput[0].title}</h2>
-              <p>{locale.logistics.compoundOutput[0].child as string}</p>
+            <ItemElement>
+              <ItemTitle margin="0 auto" padding="0">
+                {locale.logistics.compoundOutput[0].title}
+              </ItemTitle>
+              <ItemText>
+                {locale.logistics.compoundOutput[0].child as string}
+              </ItemText>
+              <Separator width="70%" />
+
               <SlideShow
                 images={[slideImg1, slideImg2, slideImg3, slideImg4, slideImg5]}
               />
-            </>
+            </ItemElement>
           }
           rightElement={
-            <>
-              <img src={rightImg} alt="" />
+            <ItemElement>
+              <ItemImg src={rightImg} />
+              <Separator />
               <div>
-                <h2>{locale.logistics.compoundOutput[1].title}</h2>
-                <p>{locale.logistics.compoundOutput[1].child as string}</p>
+                <ItemTitle margin="0 auto 2rem" padding="0">
+                  {locale.logistics.compoundOutput[1].title}
+                </ItemTitle>
+                <ItemText textCenter>
+                  {locale.logistics.compoundOutput[1].child as string}
+                </ItemText>
               </div>
-            </>
+            </ItemElement>
           }
         />
       </section>
-    </>
+    </Container>
   );
 }
