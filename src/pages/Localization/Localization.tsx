2b72fb41f4d44a5d5f288badb886a2adf3c241ca
redlintles <treelands54@gmail.com>
2024-12-10 22:20:04 -0300

    Corrigidos erros em localization

--- src/pages/Localization/Localization.tsx
+++ src/pages/Localization/Localization.tsx
@@ -12,7 +12,7 @@ const StyledLocalizationMain = styled.div`
 
   & > div {
     margin: 0 auto;
-    width: 750px;
+    width: 700px;
     display: flex;
     flex-direction: column;
     gap: 2rem;
@@ -21,23 +21,27 @@ const StyledLocalizationMain = styled.div`
 
 export default function Localization() {
   return (
-    <Container padding="0">
+    <Container padding="2rem 18rem">
       <StyledLocalizationMain>
         <div>
           <iframe
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3656.242107446491!2d-46.90473732383065!3d-23.595648562854326!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94cf00ba175e3e6f%3A0xc88580f0a400e21b!2sZanaflex%20Borrachas!5e0!3m2!1spt-BR!2sbr!4v1733624708200!5m2!1spt-BR!2sbr"
-            style={{ border: "0", width: "100%", height: "800px" }}
+            style={{ border: "0", width: "100%", height: "700px" }}
             loading="lazy"
           >
             <ItemText>Este iframe não pode ser exibido</ItemText>
           </iframe>
           <section className="localization-main__text">
-            <ItemText fontSize="1.5rem">Zanaflex &reg;</ItemText>
-            <ItemText>Zanaflex Borrachas Ltda.</ItemText>
-            <ItemText>Av. Vasco Massafeli, 1010</ItemText>
-            <ItemText>Cotia - SP - Brasil - CEP 06703-600</ItemText>
-            <ItemText>Fone : +55 (11) 2165-3450</ItemText>
-            <ItemText>Fax : +55 (11) 2165-3451</ItemText>
+            <ItemText lineHeight="1.25" fontSize="1.5rem">
+              Zanaflex &reg;
+            </ItemText>
+            <ItemText lineHeight="1.25">Zanaflex Borrachas Ltda.</ItemText>
+            <ItemText lineHeight="1.25">Av. Vasco Massafeli, 1010</ItemText>
+            <ItemText lineHeight="1.25">
+              Cotia - SP - Brasil - CEP 06703-600
+            </ItemText>
+            <ItemText lineHeight="1.25">Fone : +55 (11) 2165-3450</ItemText>
+            <ItemText lineHeight="1.25">Fax : +55 (11) 2165-3451</ItemText>
             <Link to="/">www.zanaflex.com.br</Link>
           </section>
         </div>
